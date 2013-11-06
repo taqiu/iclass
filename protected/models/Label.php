e0f60a59c6973a64ced146e8760191ef36e6be11
Tanghong Qiu <taqiu@tanghongs-mbp.gateway.2wire.net>
2013-11-06 01:55:20 -0500

    update for demo

--- protected/models/Label.php
+++ protected/models/Label.php
@@ -112,4 +112,19 @@ class Label extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+	
+
+	/**
+	 * Prepares create_time, owner, update_time and update_user_id attributes before performing validation.
+	 */
+	protected function beforeValidate()
+	{
+		if($this->isNewRecord)
+		{
+			// set the create date and the user doing the creating
+			$this->create_time = new CDbExpression('NOW()');
+			$this->owner = Yii::app()->user->id;
+		}
+		return parent::beforeValidate();
+	}
 }
--- protected/views/label/_form.php
+++ protected/views/label/_form.php
@@ -7,13 +7,9 @@
 
 <?php echo $form->errorSummary($model); ?>
 
-	<?php echo $form->textFieldRow($model,'owner',array('class'=>'span5')); ?>
-
 	<?php echo $form->textFieldRow($model,'name',array('class'=>'span5','maxlength'=>64)); ?>
 
 	<?php echo $form->textAreaRow($model,'description',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
-
-	<?php echo $form->textFieldRow($model,'create_time',array('class'=>'span5')); ?>
 
 <div class="form-actions">
 	<?php $this->widget('bootstrap.widgets.TbButton', array(
